335c07eae5dfed90e1ba0575da8c0c60a227d3b9
unknown <pussepitiyavinuka@gmail.com>
2021-10-15 14:48:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+
+//require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+//const encrypt = require("mongoose-encryption")
+//const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+//var secret = process.env.SECRET;
+
+//userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password'] })
+
+const User = mongoose.model("User", userSchema)
+
+app.get('/', function(req, res){
+  res.render("home")
+})
+
+app.get('/login', function(req, res){
+  res.render("login")
+})
+
+app.post('/login', function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, docs){
+    if(err){
+      console.log(err)
+    } else {
+      if(docs){
+        bcrypt.compare(password, docs.password, function(err, result) {
+          if(result === true) {
+          res.render('secrets')}
+            })
+        }
+      }
+    })
+})
+
+
+app.get('/register', function(req, res){
+  res.render("register")
+})
+
+app.post('/register', function(req, res){
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if(err){
+        console.log(err)
+      } else {
+        res.render('secrets')
+      }
+    })
+  });
+
+
+})
+
+
+
+app.listen(3000, function(){
+  console.log("app started at port 3000")
+})
